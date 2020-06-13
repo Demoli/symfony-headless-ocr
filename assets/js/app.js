097fa5c265108647976bcad6b3711ab1573da9b7
Neil <neil@skywire.co.uk>
2020-06-13 07:38:44 +0100

    Add axios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,14 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-import router from './router'
-// import '../scss/app.scss';
-//
-// Vue.component("v-select", vSelect);
-//
-// const axios = require("axios").create({
-//     baseURL: "http://docker.headless-store.com/api",
-//     // headers: {"X-AUTH-TOKEN": "c95352664937759188158ddf56baf9a550c00fff"},
-//     timeout: 1000,
-// });
-// Vue.prototype.$axios = axios;
-// new Vue({
-//     router,
-//     render: h => h(App)
-// }).$mount('#app')
-
 import Vue from 'vue';
 import App from './components/App';
+import router from './router'
 import '../css/app.css';
+
+const axios = require("axios").create({
+    baseURL: "http://docker.ocr-queue.com/api",
+    // headers: {"X-AUTH-TOKEN": "c95352664937759188158ddf56baf9a550c00fff"},
+    timeout: 1000,
+});
+Vue.prototype.$axios = axios;
 
 new Vue({
     router,
